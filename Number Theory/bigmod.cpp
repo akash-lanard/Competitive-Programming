ecf765731e9a1530821c4e1b1bf021c447531109
Akash Lanard <akashlanard07@gmail.com>
2017-10-16 01:40:01 +0600

    Create bigmod.cpp

---
+++ Number Theory/bigmod.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int bigmod(int a, int b, int M)
+{
+    if(b==0) return 1%M;
+    int x = bigmod(a, b/2, M);
+    x = (x*x)%M;
+    if(b%2==1) x = (x*a)%M;
+    return x;
+}
+
+int main()
+{
+    int a, b, M;
+    while(1) {
+        cin >> a >> b >> M;
+        if(a==0 && b==0 && M==0) break;
+        cout << bigmod(a, b, M) << endl;
+    }
+    return 0;
+}
