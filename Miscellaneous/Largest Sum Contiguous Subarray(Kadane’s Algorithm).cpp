78eb6267a3ed1c4775cccc89c35616201524ba08
Akash Lanard <akashlanard07@gmail.com>
2017-12-14 22:33:45 +0600

    Update Largest Sum Contiguous Subarray(Kadane’s Algorithm).cpp

--- Miscellaneous/Largest Sum Contiguous Subarray(Kadane’s Algorithm).cpp
+++ Miscellaneous/Largest Sum Contiguous Subarray(Kadane’s Algorithm).cpp
@@ -2,27 +2,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int maxSubArraySum(int a[], int sz)
+int maxSubArraySum(int a[], int size)
 {
-    int max_so_far = INT_MIN, max_ending_here = 0;
-    int strt=0, e=0, s=0;
-
-    for(int i=0; i<sz; i++) {
-        max_ending_here += a[i];
-        if(max_ending_here < 0) {
-            max_ending_here = 0;
-            s = i+1;
-        }
-        else if(max_so_far < max_ending_here) {
-            max_so_far = max_ending_here;
-            strt = s;
-            e = i;
-        }
-    }
-    /*cout << "Maximum contiguous sum is " << max_so_far << endl;
-    cout << "Starting index "<< strt << endl
-    << "Ending index "<< e << endl;*/
-    return max_so_far;
+   int max_so_far = a[0];
+   int curr_max = a[0];
+ 
+   for (int i = 1; i < size; i++)
+   {
+        curr_max = max(a[i], curr_max+a[i]);
+        max_so_far = max(max_so_far, curr_max);
+   }
+   return max_so_far;
 }
 
 int main()
