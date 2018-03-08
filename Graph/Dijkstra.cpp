86184d380fea69c9c713e820d610d9979630c645
Akash Lanard <akashlanard07@gmail.com>
2018-03-08 14:48:45 +0600

    Create Dijkstra.cpp

---
+++ Graph/Dijkstra.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SIZE 100005
+#define BIG 1000000000000005
+
+long long dis[SIZE];
+int pre[SIZE];
+
+struct Node
+{
+    int id, dist;
+
+    bool operator < (const Node &a)const {
+        return dist > a.dist;
+    }
+};
+
+vector<Node> G[SIZE];
+
+void  dijkstra(int source)
+{
+    priority_queue<Node> Q;
+
+    for(int i=0; i<SIZE; i++) {
+        dis[i] = BIG;
+        pre[i] = -1;
+    }
+
+    Node s;
+    s.dist = 0;
+    s.id = source;
+
+    Q.push(s);
+
+    dis[source] = 0;
+
+    //printf("BFS: %d", source);
+
+    while(!Q.empty()) {
+        Node curr = Q.top();
+        Q.pop();
+
+        int u = curr.id;
+
+        for(int i=0; i<G[u].size(); i++) {
+            int v = G[u][i].id;
+            int cost = G[u][i].dist;
+
+            if(dis[u]+cost < dis[v]) {
+                dis[v] = dis[u] + cost;
+
+                Node n;
+                n.id = v;
+                n.dist = dis[v];
+
+                Q.push(n);
+
+                pre[v] = u;
+
+                //printf(" %d", v);
+            }
+        }
+    }
+    //printf("\n");
+}
+
+void clr()
+{
+    for(int i=0; i<SIZE; i++) G[i].clear();
+}
+
+int main () {
+    #ifdef forthright48
+    freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    //fast_cin;
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    for(int i=0; i<m; i++) {
+        int a, b, w;
+        scanf("%d %d %d", &a, &b, &w);
+
+        Node aa, bb;
+        aa.id = a;
+        aa.dist = w;
+
+        bb.id = b;
+        bb.dist = w;
+
+        G[a].push_back(bb);
+        G[b].push_back(aa);
+    }
+
+    dijkstra(1);
+
+    if(dis[n]==BIG) {
+        printf("-1\n");
+    }
+    else {
+        vector<int> ansVec;
+
+        int curr = n;
+        while(curr!=1) {
+            ansVec.push_back(curr);
+            curr = pre[curr];
+        }
+        ansVec.push_back(1);
+
+        for(int i=ansVec.size()-1; i>=0; i--) {
+            if(i==ansVec.size()-1) printf("%d", ansVec[i]);
+            else printf(" %d", ansVec[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
