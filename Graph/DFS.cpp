999499ecc8d075ccf789d15e6a5b0fa76850b32e
Akash Lanard <akashlanard07@gmail.com>
2018-03-14 02:31:06 +0600

    Update DFS.cpp

--- Graph/DFS.cpp
+++ Graph/DFS.cpp
@@ -1,28 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define SIZE 5
+#define SIZE 50005
+
+vector<int> G[SIZE];
+
+//int visited[SIZE];
 
 int d[SIZE], f[SIZE], color[SIZE];
 int ttime;
-
 vector<int> lst;
 
-vector<int> G[SIZE];
+//int inDegree[SIZE];
+//int cnt;
+//int mark[SIZE];
 
 void DFS(int source)
 {
     int u = source;
+
+    //visited[u] = 1;
+
     ttime++;
     d[u] = ttime;
     color[u] = 1;
 
+    //cnt++;
+    //mark[u] = 1;
+
     for(int v = 0; v<G[u].size(); v++) {
-        if(color[G[u][v]]==0) DFS(G[u][v]);
+        int curr = G[u][v];
+        if(color[curr]==0) {
+            DFS(curr);
+        }
+        /*if(!visited[curr]) {
+            DFS(curr);
+        }*/
     }
 
     lst.push_back(u);
-
     color[u] = 2;
     ttime++;
     f[u] = ttime;
@@ -30,19 +46,39 @@ void DFS(int source)
     return;
 }
 
+void clr()
+{
+    for(int i=0; i<SIZE; i++) G[i].clear();
+
+    //memset(visited, 0, sizeof(visited));
+
+    memset(d, 0, sizeof(d));
+    memset(f, 0, sizeof(f));
+    memset(color, 0, sizeof(color));
+    lst.clear();
+    ttime = 0;
+
+    //memset(inDegree, 0, sizeof(inDegree));
+    //memset(mark, 0, sizeof(mark));
+}
+
 int main()
 {
+    clr();
+
+    int n = 5;
+
     G[1].push_back(4);
     G[2].push_back(1);
     G[2].push_back(3);
     G[3].push_back(4);
 
     //DFS(1);
-    for(int i=1; i<SIZE; i++) {
+    for(int i=1; i<n; i++) {
         if(color[i]==0) DFS(i);
     }
 
-    for(int i=1; i<SIZE; i++) {
+    for(int i=1; i<n; i++) {
         printf("Node: %d, d[]: %d, f[]: %d\n", i, d[i], f[i]);
     }
 
