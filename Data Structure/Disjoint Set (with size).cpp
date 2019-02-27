54afc99a5dbb6404293629a12b9f5bcd0fb23bc5
Akash Lanard <akashlanard07@gmail.com>
2019-02-27 12:16:29 +0600

    Create Disjoint Set (with size).cpp

---
+++ Data Structure/Disjoint Set (with size).cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define SIZE 150005
+
+int parent[SIZE];
+int sz[SIZE];
+
+void makeset(int u) {
+    parent[u] = u;
+    sz[u] = 1;
+}
+
+void init_disjoint_set(int N) {
+    for(int i=0; i<=N; i++) {
+        makeset(i);
+    }
+}
+
+int FindRepresentative(int r) {  //with path compression
+    if(parent[r]==r) {
+        return r;
+    }
+
+    parent[r] = FindRepresentative(parent[r]);
+
+    return parent[r];
+}
+
+int FindSize(int r) {
+    return sz[FindRepresentative(r)];
+}
+
+void Union(int a, int b) {
+    int u = FindRepresentative(a);
+    int v = FindRepresentative(b);
+
+    if(u!=v) {
+        parent[u] = v;
+        sz[v] += sz[u];
+    }
+}
+
+int main () {
+    #ifdef forthright48
+    freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    //fast_cin;
+
+    init_disjoint_set(10);
+
+    Union(1, 2);
+    Union(1, 3);
+
+    Union(4, 5);
+
+    Union(2, 5);
+
+    cout << FindSize(5) << endl;
+
+    return 0;
+}
+
