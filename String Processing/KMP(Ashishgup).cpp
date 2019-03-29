eae694ac0d27a053094152afbd577b8982236e14
Akash Lanard <akashlanard07@gmail.com>
2019-03-29 07:33:49 +0600

    Create KMP(Ashishgup).cpp

---
+++ String Processing/KMP(Ashishgup).cpp
@@ -0,0 +1,226 @@
+///====================== TEMPLATE STARTS HERE =====================///
+#include <bits/stdc++.h>
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp> // Include for built in treap
+//#include <ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+
+//#include <sys/resource.h>     // for linux stack memory increase
+//#define gc getchar_unlocked   // for linux fast io
+//#define gc getchar            // for windows fast io
+
+#define pb push_back
+#define MP make_pair
+#define ff first
+#define ss second
+#define nl puts("")
+#define sp printf(" ")
+#define phl debug("Hello")
+#define FOR(i,x,y) for(vlong i = (x) ; i <= (y) ; ++i)
+#define ROF(i,x,y) for(vlong i = (y) ; i >= (x) ; --i)
+#define CLR(x,y) memset(x,y,sizeof(x))
+#define ALL(x) (x).begin(),(x).end()
+#define SZ(x) ((vlong)(x).size())
+#define UNIQUE(V) (V).erase(unique((V).begin(),(V).end()),(V).end())
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+#define ABS(x) ((x)<0?-(x):(x))
+#define FABS(x) ((x)+eps<0?-(x):(x))
+#define SQ(x) ((x)*(x))
+#define LCM(x,y) (((x)/gcd((x),(y)))*(y))
+#define POPCOUNT __builtin_popcountll
+#define RIGHTMOST __builtin_ctzll
+#define LEFTMOST(x) (63-__builtin_clzll((x)))
+#define NUMDIGIT(x,y) (((vlong)(log10((x))/log10((y))))+1)
+#define NORM(x) if(x>=mod) x-=mod;if(x<0) x+=mod;
+#define ODD(x) (((x)&1)==0?(0):(1))
+#define Set(N,p) N=((N)|((1LL)<<(p)))
+#define Reset(N,p) N=((N)&(~((1LL)<<(p))))
+#define Check(N,p) (!(((N)&((1LL)<<(p)))==(0)))
+#define fast_cin ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define arrayIndexPrint(A,i) cerr<<"~ At pos: "<<i<<" = "<<A[i]
+#define dump(x) cerr<<"~ "<<#x<<" = "<<x<<endl
+#define arrayPrint(A,st,ed) cerr<<"~ Array:";FOR(i,st,ed) cerr<<" "<<A[i];cerr<<endl
+#define LINE cerr<<"\n"; FOR(i,0,50) cerr<<"=";cerr<<"\n\n"
+
+#define LL long long
+#define LLU long long unsigned int
+typedef long long vlong;
+typedef unsigned long long uvlong;
+typedef pair < int, int > pii;
+typedef pair < vlong, vlong > pll;
+typedef vector<int> vi;
+typedef vector<vlong> vl;
+typedef vector<pll> vll;
+//typedef tree <int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pb_ds;
+
+#ifdef forthright48
+     #include <ctime>
+     clock_t tStart = clock();
+     #define debug(args...) {dbg,args; cerr<<endl;}
+     #define timeStamp debug ("Execution Time: ", (double)(clock() - tStart)/CLOCKS_PER_SEC)
+     #define bug printf("%d\n",__LINE__);
+
+#else
+    #define debug(args...)  // Just strip off all debug tokens
+    #define timeStamp
+#endif
+
+struct debugger{
+    template<typename T> debugger& operator , (const T& v){
+        cerr<<v<<" ";
+        return *this;
+    }
+}dbg;
+
+inline vlong gcd ( vlong a, vlong b ) {
+    a = ABS ( a ); b = ABS ( b );
+    while ( b ) { a = a % b; swap ( a, b ); } return a;
+}
+
+vlong ext_gcd ( vlong A, vlong B, vlong *X, vlong *Y ){
+    vlong x2, y2, x1, y1, x, y, r2, r1, q, r;
+    x2 = 1; y2 = 0;
+    x1 = 0; y1 = 1;
+    for (r2 = A, r1 = B; r1 != 0; r2 = r1, r1 = r, x2 = x1, y2 = y1, x1 = x, y1 = y ) {
+        q = r2 / r1;
+        r = r2 % r1;
+        x = x2 - (q * x1);
+        y = y2 - (q * y1);
+    }
+    *X = x2; *Y = y2;
+    return r2;
+}
+
+inline vlong modInv ( vlong a, vlong m ) {
+    vlong x, y;
+    ext_gcd( a, m, &x, &y );
+    x %= m;
+    if ( x < 0 ) x += m;
+    return x;
+}
+
+inline vlong bigmod ( vlong a, vlong p, vlong m ) {
+    vlong res = 1 % m, x = a % m;
+    while ( p ) {
+        if ( p & 1 ) res = ( res * x ) % m;
+        x = ( x * x ) % m; p >>= 1; /// For bigmod2 multiply here using mulmod
+    }
+    return res;
+}
+
+
+//int knightDir[8][2] = { {-2,1},{-1,2},{1,2},{2,1},{2,-1},{-1,-2},{1,-2},{-2,-1} };
+//int dir4[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
+//int dir8[8][2] = {{-1,0},{0,1},{1,0},{0,-1},{-1,-1},{1,1},{1,-1},{-1,1}};
+const vlong inf = 2147383647;
+const vlong mod = 1000000007;
+const double pi = 2 * acos ( 0.0 );
+const double eps = 1e-11;
+
+///======================  TEMPLATE ENDS HERE  =====================///
+
+
+//String:
+
+vector<int> pre, occ;
+
+vector<int> prefix_function(string &s)
+{
+	int n = (int)s.length();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++)
+	{
+		int j = pi[i-1];
+		while (j > 0 && s[i] != s[j])
+			j = pi[j-1];
+		if (s[i] == s[j])
+			j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+vector<int> find_occurences(string &text, string &pattern)
+{
+	string cur=pattern + '#' + text;
+	int sz1=text.size(), sz2=pattern.size();
+	vector<int> v;
+	vector<int> lps=prefix_function(cur);
+	for(int i=sz2+1;i<=sz1+sz2;i++)
+	{
+		if(lps[i]==sz2)
+			v.push_back(i-2*sz2);
+	}
+	return v;
+}
+
+//Vector:
+
+vector<int> prefix_function(vector<int> &v)
+{
+	int n = (int)v.size();
+	vector<int> pi(n);
+	for (int i = 1; i < n; i++)
+	{
+		int j = pi[i-1];
+		while (j > 0 && v[i] != v[j])
+			j = pi[j-1];
+		if (v[i] == v[j])
+			j++;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+vector<int> find_occurences(vector<int> &text, vector<int> &pattern)
+{
+	vector<int> v=pattern;
+	v.push_back(-1);
+	for(auto &it:text)
+		v.push_back(it);
+	int sz1=text.size(), sz2=pattern.size();
+	vector<int> lps=prefix_function(v);
+	vector<int> store;
+	for(int i=sz2+1;i<=sz1+sz2;i++)
+	{
+		if(lps[i]==sz2)
+			store.push_back(i-sz2);
+	}
+	return v;
+}
+
+
+//Problem 1 (Basic KMP): https://codeforces.com/contest/1016/problem/B
+//Solution 1: https://codeforces.com/contest/1016/submission/41167402
+
+//Problem 2 and Solution (Cyclic Matching - Vectors): http://codeforces.com/gym/100502/submission/41562571
+
+//Problem 3 (Ranking Pattern): https://qr.ae/TUG5h9
+//Solution 3: http://p.ip.fi/Hwpy
+
+int main () {
+    #ifdef forthright48
+    freopen ( "input.txt", "r", stdin );
+    //freopen ( "output.txt", "w", stdout );
+    #endif // forthright48
+
+    fast_cin;
+
+    int tc;
+    cin >> tc;
+
+    for(int kase=0; kase<tc; kase++) {
+        string pattern, text;
+        cin >> pattern >> text;
+
+        pre = prefix_function(pattern);
+
+        occ = find_occurences(text, pattern);
+
+        cout << occ.size() << "\n";
+    }
+
+    return 0;
+}
