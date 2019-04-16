8e6e321db7a147c5a401f1eed475cbec369d1acd
Akash Lanard <akashlanard07@gmail.com>
2019-04-17 01:44:15 +0600

    Update CircleIntersect.cpp

--- Geometry/CircleIntersect.cpp
+++ Geometry/CircleIntersect.cpp
@@ -1,6 +1,5 @@
 int circleIntersect(LL x1, LL y1, LL x2, LL y2, LL r1, LL r2)  {
-    LL distSq = (x1 - x2) * (x1 - x2) +
-                 (y1 - y2) * (y1 - y2);
+    LL distSq = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
     LL radSumSq = (r1 + r2) * (r1 + r2);
     if (distSq == radSumSq) return 1; // touches
     else if (distSq > radSumSq) return -1; // does not touch or intersect 
