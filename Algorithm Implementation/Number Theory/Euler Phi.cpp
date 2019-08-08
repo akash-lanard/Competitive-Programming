109c2e87eece9c327b8077049c9ff81f8c06e708
Akash Lanard <akashlanard07@gmail.com>
2019-08-09 03:33:09 +0600

    Update Euler Phi.cpp

--- Algorithm Implementation/Number Theory/Euler Phi.cpp
+++ Algorithm Implementation/Number Theory/Euler Phi.cpp
@@ -1,13 +1,13 @@
 #define SIZE 1000005
 int phi[SIZE];
 
-void euler_phi(int n){
+void euler_phi(int n) {
     phi[1] = 1;
-    for(int i = 2;i<=n;i++){
-        if(!phi[i]){
+    for(int i = 2;i<=n;i++) {
+        if(!phi[i]) {
             phi[i] = i-1;
-            for(int j = (i<<1);j<=n;j+=i){
-                if(!phi[j]){
+            for(int j = (i<<1);j<=n;j+=i) {
+                if(!phi[j]) {
                     phi[j] = j;
                 }
                 phi[j] = phi[j]/i*(i-1);
