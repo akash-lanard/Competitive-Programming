315cad14ade605f51ce8593487e58e7c17997708
Akash Lanard <akashlanard07@gmail.com>
2019-08-09 04:29:09 +0600

    Update and rename Miscellaneous/isleap.cpp to Algorithm Implementation/Miscellaneous/isleap.cpp

--- Algorithm Implementation/Miscellaneous/isleap.cpp
+++ Algorithm Implementation/Miscellaneous/isleap.cpp
@@ -1,13 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int isleap(int y)
-{
+int isleap(int y) {
     return y%4==0 && (y%100 != 0 || y%400 == 0);
 }
 
-int main()
-{
+int main() {
     int n;
     scanf("%d", &n);
     printf("%d\n", isleap(n));
