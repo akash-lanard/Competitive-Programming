781a76123f706cf996771b1bb3893fbb2208f04c
Akash Lanard <akashlanard07@gmail.com>
2019-08-09 03:48:20 +0600

    Update and rename Number Theory/mulmod.cpp to Algorithm Implementation/Number Theory/mulmod.cpp

--- Algorithm Implementation/Number Theory/mulmod.cpp
+++ Algorithm Implementation/Number Theory/mulmod.cpp
@@ -1,18 +1,15 @@
 
-// C++ program for modular multiplication without 
-// any overflow 
+// C++ program for modular multiplication without any overflow 
 #include<iostream> 
 using namespace std; 
   
 typedef long long int LL; 
   
 // To compute (a * b) % mod 
-LL mulmod(LL a, LL b, LL mod) 
-{ 
+LL mulmod(LL a, LL b, LL mod) { 
     LL res = 0; // Initialize result 
     a = a % mod; 
-    while (b > 0) 
-    { 
+    while (b > 0) { 
         // If b is odd, add 'a' to result 
         if (b % 2 == 1) 
             res = (res + a) % mod; 
@@ -29,8 +26,7 @@ LL mulmod(LL a, LL b, LL mod)
 } 
   
 // Driver program 
-int main() 
-{ 
+int main()  { 
    ll a = 9223372036854775807, b = 9223372036854775807; 
    cout << mulmod(a, b, 100000000000); 
    return 0; 
