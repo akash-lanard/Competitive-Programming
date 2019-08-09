ddb00620dfeaad235e3168ce472cd9b503219556
Akash Lanard <akashlanard07@gmail.com>
2019-08-09 23:48:22 +0600

    Update Largest rectangle in a histogram.cpp

--- Algorithm Implementation/Miscellaneous/Largest rectangle in a histogram.cpp
+++ Algorithm Implementation/Miscellaneous/Largest rectangle in a histogram.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int findMaxArea(int ara[], int n)  { //Note: may require long long for larger inputs
-    stack<int> s;
+    stack<int> s; // idea is to maintain non decreasing stack
     int i=0;
     int maxArea = 0;
     while(i<n) {
         if(s.empty() || ara[s.top()]<=ara[i]) {
-            s.push(i++);
-        }
+            s.push(i++); // add to stack if current value is bigger or equal to the top
+        }                // or the stack is empty
         else {
-            int top_indx = s.top();
-            s.pop();
+            int top_indx = s.top(); // otherwise keep removing items until current value is bigger or
+            s.pop();                // equal to the top. Calculate area and update max every time.
             int area = ara[top_indx]*(s.empty() ? i : i-s.top()-1);
             if(area>maxArea) maxArea = area;
         }
     }
 
-    while(!s.empty()) {
-        int top_indx = s.top();
+    while(!s.empty()) {             // process the remaining items in the stack in the same way.
+        int top_indx = s.top();     // it handles the cases from right to left.
         s.pop();
         int area = ara[top_indx]*(s.empty() ? i : i-s.top()-1);
         if(area>maxArea) maxArea = area;
