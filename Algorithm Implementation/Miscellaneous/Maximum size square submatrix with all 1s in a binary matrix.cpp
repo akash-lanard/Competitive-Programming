a4aca5e2ead8dea51920f77d70c33521cf34023b
Akash Lanard <akashlanard07@gmail.com>
2019-08-10 02:11:11 +0600

    Update Maximum size square submatrix with all 1s in a binary matrix.cpp

--- Algorithm Implementation/Miscellaneous/Maximum size square submatrix with all 1s in a binary matrix.cpp
+++ Algorithm Implementation/Miscellaneous/Maximum size square submatrix with all 1s in a binary matrix.cpp
@@ -1,3 +1,19 @@
+// Reference: https://www.geeksforgeeks.org/maximum-size-sub-matrix-with-all-1s-in-a-binary-matrix/
+// This is a dynamic programming based solution. 
+/*
+Pseudo code:
+1) Construct a sum matrix S[R][C] for the given M[R][C].
+     a)    Copy first row and first columns as it is from M[][] to S[][]
+     b)    For other entries, use following expressions to construct S[][]
+         If M[i][j] is 1 then
+            S[i][j] = min(S[i][j-1], S[i-1][j], S[i-1][j-1]) + 1
+         Else If M[i][j] is 0
+            S[i][j] = 0
+2) Find the maximum entry in S[R][C]
+3) Using the value and coordinates of maximum entry in S[i], print 
+   sub-matrix of M[][]
+*/
+
 #define SIZE 505
 
 int n, m;
