8ff5ab1da84afd2aa48bdb9b39cd5b6595406a66
Akash Lanard <akashlanard07@gmail.com>
2019-08-29 15:29:15 +0600

    Update Strongly Connected Components (Kosaraju).cpp

--- Algorithm Implementation/Graph/Strongly Connected Components (Kosaraju).cpp
+++ Algorithm Implementation/Graph/Strongly Connected Components (Kosaraju).cpp
@@ -1,82 +1,82 @@
-// Reference: https://www.geeksforgeeks.org/strongly-connected-components/
+/// Reference: https://www.geeksforgeeks.org/strongly-connected-components/
 
 #include <bits/stdc++.h>
 using namespace std;
 
 #define SIZE 100005
 
-vector<int> G[SIZE];// Graph
-vector<int> T[SIZE];// Transpose of the Graph
-int V;              // number of vertices
-int E;              // number of edges
-int visited[SIZE];  // visited array
-stack<int> S;       // stack (stores vertices with increasing order of finishing time)
-                    // (top has the maximum finishing time)
+vector<int> G[SIZE];/// Graph
+vector<int> T[SIZE];/// Transpose of the Graph
+int V;              /// number of vertices
+int E;              /// number of edges
+int visited[SIZE];  /// visited array
+stack<int> S;       /// stack (stores vertices with increasing order of finishing time)
+                    /// (top has the maximum finishing time)
 
-// A recursive function to print DFS starting from v
+/// A recursive function to print DFS starting from v
 void dfs(int curr) {
-    visited[curr] = true;   // Mark the current node as visited and print it
+    visited[curr] = true;   /// Mark the current node as visited and print it
     cout << curr << " ";
 
     for(int i=0; i<T[curr].size(); i++) {
         int next = T[curr][i];
-        if(!visited[next]) {// Recur for all the vertices adjacent to this vertex
+        if(!visited[next]) {/// Recur for all the vertices adjacent to this vertex
             dfs(next);
         }
     }
 }
 
-void getTranspose() {           // Generates the transpose of the Graph
+void getTranspose() {           /// Generates the transpose of the Graph
     for(int i=0; i<SIZE; i++) {
-        T[i].clear();           // clear the transpose graph
+        T[i].clear();           /// clear the transpose graph
     }
 
-    for(int i=0; i<V; i++) {    // 0 indexed
+    for(int i=0; i<V; i++) {    /// 0 indexed
         for(int j=0; j<G[i].size(); j++) {
-            int u = i;          // from (in the original graph)
-            int v = G[i][j];    // to (in the original graph)
+            int u = i;          /// from (in the original graph)
+            int v = G[i][j];    /// to (in the original graph)
 
-            T[v].push_back(u);  // to to from (reversed)
+            T[v].push_back(u);  /// to to from (reversed)
         }
     }
 }
 
-// Fills Stack with vertices (in increasing order of finishing times)
-// The top element of stack has the maximum finishing time
+/// Fills Stack with vertices (in increasing order of finishing times)
+/// The top element of stack has the maximum finishing time
 void fillStack(int curr) {
-    visited[curr] = true;       // Mark the current node as visited
+    visited[curr] = true;       /// Mark the current node as visited
 
     for(int i=0; i<G[curr].size(); i++) {
-        int next = G[curr][i];  // Recur for all the vertices adjacent to this vertex
+        int next = G[curr][i];  /// Recur for all the vertices adjacent to this vertex
         if(!visited[next]) {
             fillStack(next);
         }
     }
 
-    S.push(curr);               // All vertices reachable from v are processed by now, push v
+    S.push(curr);               /// All vertices reachable from v are processed by now, push v
 }
 
 void GetSCCs() {
-    while(!S.empty()) S.pop();          // empty the stack
+    while(!S.empty()) S.pop();          /// empty the stack
 
-    memset(visited, 0, sizeof(visited));// Mark all the vertices as not visited (For first DFS)
+    memset(visited, 0, sizeof(visited));/// Mark all the vertices as not visited (For first DFS)
 
-    for(int i=0; i<V; i++) {            // 0 indexed
-        if(!visited[i]) {               // Fill vertices in stack according to their finishing times
+    for(int i=0; i<V; i++) {            /// 0 indexed
+        if(!visited[i]) {               /// Fill vertices in stack according to their finishing times
             fillStack(i);
         }
     }
 
-    getTranspose();                     // Generate the reversed graph T
+    getTranspose();                     /// Generate the reversed graph T
 
-    memset(visited, 0, sizeof(visited));// Mark all the vertices as not visited (For second DFS)
+    memset(visited, 0, sizeof(visited));/// Mark all the vertices as not visited (For second DFS)
 
-    while(!S.empty()) {                 // Now process all vertices in order defined by Stack
+    while(!S.empty()) {                 /// Now process all vertices in order defined by Stack
         int curr = S.top();
-        S.pop();                        // Pop a vertex from stack
+        S.pop();                        /// Pop a vertex from stack
 
         if(!visited[curr]) {
-            dfs(curr);                  // Print Strongly connected component of the popped vertex
+            dfs(curr);                  /// Print Strongly connected component of the popped vertex
             cout << "\n";
         }
     }
