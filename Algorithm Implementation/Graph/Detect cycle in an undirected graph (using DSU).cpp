5711eb91651ad10e0efb808e2bdabbc7b55bfe95
Akash Lanard <akashlanard07@gmail.com>
2019-08-29 15:13:16 +0600

    Update Detect cycle in an undirected graph (using DSU).cpp

--- Algorithm Implementation/Graph/Detect cycle in an undirected graph (using DSU).cpp
+++ Algorithm Implementation/Graph/Detect cycle in an undirected graph (using DSU).cpp
@@ -1,7 +1,7 @@
-// Reference: https://www.geeksforgeeks.org/union-find/
-// Detects cycle in an undirected graph
-// Notice: we assume that the graph does not contain any self loop
-// and does not have multiple edge (simple graph). It also handles for disconnected graph.
+/// Reference: https://www.geeksforgeeks.org/union-find/
+/// Detects cycle in an undirected graph
+/// Notice: we assume that the graph does not contain any self loop
+/// and does not have multiple edge (simple graph). It also handles for disconnected graph.
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -10,10 +10,10 @@ using namespace std;
 
 typedef pair<int, int> pii;
 
-int n, m; // number of nodes, number of edges
-vector<pii> edgeVec;    // vector containing edges
+int n, m; /// number of nodes, number of edges
+vector<pii> edgeVec;    /// vector containing edges
 
-// disjoint set codes
+/// disjoint set codes
 int parent[SIZE];
 
 void makeset(int u) {
@@ -26,14 +26,14 @@ void init_disjoint_set(int N) {
     }
 }
 
-/*int FindRepresentative(int r) { //without path compression
+/*int FindRepresentative(int r) { ///without path compression
     if(parent[r]==r) {
         return r;
     }
     return FindRepresentative(parent[r]);
 }*/
 
-int FindRepresentative(int r) { //with path compression
+int FindRepresentative(int r) { ///with path compression
     if(parent[r]==r) {
         return r;
     }
@@ -51,21 +51,21 @@ void Union(int a, int b) {
         parent[u] = v;
     }
 }
-// end of disjoint set code
+/// end of disjoint set code
 
-bool isCyclic() { // checks if the graph contains cycle
+bool isCyclic() { /// checks if the graph contains cycle
     init_disjoint_set(n+1);
 
     for(int i=0; i<edgeVec.size(); i++) {
         int x = FindRepresentative(edgeVec[i].first);
         int y = FindRepresentative(edgeVec[i].second);
 
-        if(x==y) return true; // if both the nodes are in the same set, cycle exists
+        if(x==y) return true; /// if both the nodes are in the same set, cycle exists
 
         Union(x, y);
     }
 
-    return false;   // no cycle
+    return false;   /// no cycle
 }
 
 int main() {
