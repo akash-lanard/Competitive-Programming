687e0193c7df7a8a9238166de03c5785b8216035
Akash Lanard <akashlanard07@gmail.com>
2019-08-29 15:11:57 +0600

    Update Detect cycle in an undirected graph (using DFS).cpp

--- Algorithm Implementation/Graph/Detect cycle in an undirected graph (using DFS).cpp
+++ Algorithm Implementation/Graph/Detect cycle in an undirected graph (using DFS).cpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/*
-We can use DFS to detect cycle in an undirected graph in O(V+E) time.
-We do a DFS traversal of the given graph. For every visited vertex ‘v’,
-if there is an adjacent ‘u’ such that u is already visited and u is not parent of v,
-then there is a cycle in graph. If we don’t find such an adjacent for any vertex,
-we say that there is no cycle. The assumption of this approach is that there are
-no parallel edges between any two vertices.
-*/
+/// We can use DFS to detect cycle in an undirected graph in O(V+E) time.
+/// We do a DFS traversal of the given graph. For every visited vertex ‘v’,
+/// if there is an adjacent ‘u’ such that u is already visited and u is not parent of v,
+/// then there is a cycle in graph. If we don’t find such an adjacent for any vertex,
+/// we say that there is no cycle. The assumption of this approach is that there are
+/// no parallel edges between any two vertices.
 
 #define SIZE 100005
 
