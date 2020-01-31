857783f5431e71bda1c952be24324fc43e88db18
Akash Lanard <akashlanard07@gmail.com>
2020-01-31 22:53:46 +0600

    Update Template.cpp

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -74,6 +74,24 @@ struct debugger{
     }
 }dbg;
 
+#ifdef forthright48
+    #define line                    cout << "\n===================\n"
+    #define trace(...)              __f( #__VA_ARGS__ , __VA_ARGS__ )
+    template <typename Arg1>
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " = " << arg1 << "\n";
+    }
+    template <typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');
+        cerr.write(names, comma - names) << " = " << arg1 << " , ";
+    __f(comma + 1, args...);
+    }
+#else
+    #define line
+    #define trace(...)
+#endif
+
 inline vlong gcd ( vlong a, vlong b ) {
     a = ABS ( a ); b = ABS ( b );
     while ( b ) { a = a % b; swap ( a, b ); } return a;
@@ -129,7 +147,9 @@ int main () {
     //freopen ( "output.txt", "w", stdout );
     #endif // forthright48
 
-    //fast_cin;
+    fast_cin;
+
+
 
     return 0;
 }
