e06461b792ec6e696e8b47677c990312fa20ce83
Akash Lanard <akashlanard07@gmail.com>
2020-05-09 20:17:53 +0600

    Update BFS 2D grid.cpp

--- Algorithm Implementation/Graph/BFS 2D grid.cpp
+++ Algorithm Implementation/Graph/BFS 2D grid.cpp
@@ -1,27 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef pair<int, int> pii;
+
+#define SIZE 1005
+
 int fx[] = {1, -1, 0, 0};
 int fy[] = {0, 0, 1, -1};
 
-int cell[100][100];
+int cell[SIZE][SIZE];
 
-int d[100][100], vis[100][100];
+int d[SIZE][SIZE], vis[SIZE][SIZE];
 
 int row, col;
 
-void BFS2D(int sx, int sy)
-{
+queue<pii> q;
+
+void BFS2D(int sx, int sy) {
     memset(vis, 0, sizeof(vis));
     vis[sx][sy] = 1;
 
     memset(d, 0, sizeof(d));
 
-    queue<pair<int, int> > q;
-    q.push(pair<int, int>(sx, sy));
+    while(!q.empty()) q.pop();
+    q.push(pii(sx, sy));
 
     while(!q.empty()) {
-        pair<int, int> top = q.front();
+        pii top = q.front();
         q.pop();
         for(int k=0; k<4; k++) {
             int tx = top.first + fx[k];
@@ -29,15 +34,14 @@ void BFS2D(int sx, int sy)
             if(tx>=0 && tx<row && ty>=0 && ty<col && cell[tx][ty]!=-1 && vis[tx][ty]==0) {
                 vis[tx][ty] = 1;
                 d[tx][ty] = d[top.first][top.second] + 1;
-                q.push(pair<int, int>(tx, ty));
+                q.push(pii(tx, ty));
             }
         }
     }
 
 }
 
-void clr()
-{
+void clr() {
     memset(cell, 0, sizeof(cell));
 }
 
