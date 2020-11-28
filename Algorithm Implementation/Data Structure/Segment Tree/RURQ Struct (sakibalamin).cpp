b0578996842f7954e0e16308c7daf74e232853b5
Akash Lanard <akashlanard07@gmail.com>
2020-11-28 16:43:05 +0600

    Update RURQ Struct (sakibalamin).cpp

--- Algorithm Implementation/Data Structure/Segment Tree/RURQ Struct (sakibalamin).cpp
+++ Algorithm Implementation/Data Structure/Segment Tree/RURQ Struct (sakibalamin).cpp
@@ -16,10 +16,10 @@ struct SEGMENT_TREE {
     }
     inline void LazyPropagate(int node, int b, int e) {
         if(Lazy[node] != 0) {
-            Tree[node] += (Lazy[node] * (e-b+1));
+            Tree[node] += (Lazy[node] * (e-b+1));       /// change here
             if(b != e) {
-                Lazy[2*node] += Lazy[node];
-                Lazy[2*node + 1] += Lazy[node];
+                Lazy[2*node] += Lazy[node];             /// change here
+                Lazy[2*node + 1] += Lazy[node];         /// change here
             }
             Lazy[node] = 0;
         }
@@ -30,32 +30,32 @@ struct SEGMENT_TREE {
         int left = node << 1, right = left | 1, mid = (b+e) >> 1;
         Build(left, b, mid);
         Build(right, mid+1, e);
-        Tree[node] = Tree[left] + Tree[right];
+        Tree[node] = Tree[left] + Tree[right];          /// change here
     }
     inline void Update(int i, int j, int val, int node = 1, int b = 1, int e = N) {
         LazyPropagate(node, b, e);
         if(i > e || j < b || b > e) return;
         if(i <= b && j >= e) {
-            Tree[node] += (val * (e-b+1));
+            Tree[node] += (val * (e-b+1));              /// change here
             if(b != e) {
-                Lazy[2*node] += val;
-                Lazy[2*node + 1] += val;
+                Lazy[2*node] += val;                    /// change here
+                Lazy[2*node + 1] += val;                /// change here
             }
             return;
         }
         int left = node << 1, right = left | 1, mid = (b+e) >> 1;
         Update(i, j, val, left, b, mid);
         Update(i, j, val, right, mid + 1, e);
-        Tree[node] = Tree[left] + Tree[right];
+        Tree[node] = Tree[left] + Tree[right];          /// change here
     }
     inline int Query(int i, int j, int node = 1, int b = 1, int e = N) {
         LazyPropagate(node, b, e);
-        if(i > e || j < b || b > e) return 0;
+        if(i > e || j < b || b > e) return 0;           /// change here
         if(i <= b && j >= e) return Tree[node];
         int left = node << 1, right = left | 1, mid = (b+e) >> 1;
         int Q1 = Query(i, j, left, b, mid);
         int Q2 = Query(i, j, right, mid+1, e);
-        return (Q1 + Q2);
+        return (Q1 + Q2);                               /// change here
     }
 
 } ST;
