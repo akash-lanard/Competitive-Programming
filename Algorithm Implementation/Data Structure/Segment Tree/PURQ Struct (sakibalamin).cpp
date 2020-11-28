e77860ae8feb229606044792542a31941feed2e0
Akash Lanard <akashlanard07@gmail.com>
2020-11-28 16:29:10 +0600

    Update PURQ Struct (sakibalamin).cpp

--- Algorithm Implementation/Data Structure/Segment Tree/PURQ Struct (sakibalamin).cpp
+++ Algorithm Implementation/Data Structure/Segment Tree/PURQ Struct (sakibalamin).cpp
@@ -8,16 +8,15 @@
 int N, Q;
 
 struct SegmentTree {
-    int Tree[4*SIZE];
-    int arr[SIZE];
+    int arr[SIZE], Tree[4*SIZE];
 
     inline void Init() {
         memset(Tree, 0, sizeof(Tree));
     }
     inline void Build(int node = 1, int b = 1, int e = N) {
         if(b > e) return;
-        if(b == e) { Tree[ node ] = arr[ b ]; return; }
-        int left = node << 1 , right = left | 1 , mid = ( b+e ) >> 1;
+        if(b == e) { Tree[node] = arr[b]; return; }
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
         Build(left, b, mid);
         Build(right, mid+1, e);
         Tree[node] = Tree[left] + Tree[right];
@@ -25,7 +24,7 @@ struct SegmentTree {
     inline void Update(int pos, int val, int node = 1, int b = 1, int e = N) {
         if(pos > e || pos < b || b > e) return;
         if(b == e && b == pos) { Tree[node] = arr[b] = val; return; }
-        int left = node << 1 , right = left | 1 , mid = ( b+e ) >> 1;
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
         Update(pos, val, left, b, mid);
         Update(pos, val, right, mid+1, e);
         Tree[node] = Tree[left] + Tree[right];
@@ -33,7 +32,7 @@ struct SegmentTree {
     inline int Query(int i, int j, int node = 1, int b = 1, int e = N) {
         if(i > e || j < b || b > e) return 0;
         if(i <= b && j >= e) return Tree[node];
-        int left = node << 1 , right = left | 1 , mid = ( b+e ) >> 1;
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
         int Q1 = Query(i, j, left, b, mid);
         int Q2 = Query(i, j, right, mid+1, e);
         return (Q1 + Q2);
