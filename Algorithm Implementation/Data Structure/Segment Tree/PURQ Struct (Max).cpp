596a5ea0593e76850d1d2bb95665b7e1bd6a3505
Akash Lanard <akashlanard07@gmail.com>
2021-01-11 18:38:15 +0600

    Create PURQ Struct (Max).cpp

---
+++ Algorithm Implementation/Data Structure/Segment Tree/PURQ Struct (Max).cpp
@@ -0,0 +1,35 @@
+#define SIZE 100005
+
+int N, Q;
+
+struct SegmentTree {
+    int arr[SIZE], Tree[4*SIZE];
+
+    inline void Init() {
+        memset(Tree, 0, sizeof(Tree));
+    }
+    inline void Build(int node = 1, int b = 1, int e = N) {
+        if(b > e) return;
+        if(b == e) { Tree[node] = arr[b]; return; }
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
+        Build(left, b, mid);
+        Build(right, mid+1, e);
+        Tree[node] = max(Tree[left], Tree[right]);              /// change here
+    }
+    inline void Update(int pos, int val, int node = 1, int b = 1, int e = N) {
+        if(pos > e || pos < b || b > e) return;
+        if(b == e && b == pos) { Tree[node] = arr[b] = val; return; }
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
+        Update(pos, val, left, b, mid);
+        Update(pos, val, right, mid+1, e);
+        Tree[node] = max(Tree[left], Tree[right]);              /// change here
+    }
+    inline int Query(int i, int j, int node = 1, int b = 1, int e = N) {
+        if(i > e || j < b || b > e) return -inf;               /// change here
+        if(i <= b && j >= e) return Tree[node];
+        int left = node << 1 , right = left | 1 , mid = (b+e) >> 1;
+        int Q1 = Query(i, j, left, b, mid);
+        int Q2 = Query(i, j, right, mid+1, e);
+        return max(Q1, Q2);                                   /// change here
+    }
+} ST;
